d26fc9efc8229e023e7c2007e177357380998a0a
lev1992 <abesadzelevani1992@gmail.com>
2026-04-23 13:44:23 +0400

    deploy vmss

--- terraform/modules/vmss/outputs.tf
+++ terraform/modules/vmss/outputs.tf
@@ -1,3 +1,8 @@
+output "network_security_group_id" {
+  description = "ID of the VMSS network security group"
+  value       = azurerm_network_security_group.main.id
+}
+
 output "vmss_id" {
   description = "ID of the Linux VM scale set"
   value       = azurerm_linux_virtual_machine_scale_set.main.id
