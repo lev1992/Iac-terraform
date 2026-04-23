fdf6feabf955964d89502795157de0c3152e4ef2
lev1992 <abesadzelevani1992@gmail.com>
2026-04-23 14:05:09 +0400

    deploy vmss

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "public_ip_address" {
   description = "Public IP address of the load balancer"
   value       = module.my_vmss.public_ip_address
 }
+
+output "ssh_nat_port_range" {
+  description = "Load balancer public port range for SSH to VMSS instances"
+  value       = module.my_vmss.ssh_nat_port_range
+}
